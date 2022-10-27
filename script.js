54f55e3ae50d9424d9a642dfc4de31a6343823f7
fullstackraven <fullstackraven@gmail.com>
2022-10-27 00:11:00 -0400

    Change game & UI layout, vars, and functions

--- script.js
+++ script.js
@@ -1,77 +1,122 @@
-// Game
+//Game
+let compChoice = {Value: ""};
+let playerChoice;
+let compChoiceInt = 0;
+let playerChoiceInt = 0;
+const buttons = document.querySelectorAll('.btn');
 
-// This will execute a randomly generated game choice
-function getComputerChoice() {
-    const randomNum = Math.floor(Math.random()*3);
-    switch (randomNum) {
-        case 0:
-            return 'rock';
-        case 1: 
-            return 'paper';
-        case 2:
-            return 'scissors';
+let playerScore = 0;
+let compScore = 0;
+let roundCount = 0; 
+
+const player = document.querySelector("#player-score");
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector("#comp-score");
+computer.textContent = `Computer Score: ${compScore}`;
+
+const output = document.querySelector("#output");
+output.textContent = "May the best win!";
+
+const reset = document.querySelector('#roundReset');
+reset.textContent = "Click Rock, Paper or Scissors to play!";
+
+const round = document.querySelector('#roundCount');
+round.textContent = ' ';
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('clicked');
+}
+
+
+buttons.forEach((button)=>{button.addEventListener('click',()=>{
+
+    playerChoice = button.id;
+    if (playerChoice == "rock"){
+        playerChoiceInt = 0;
+    }
+    else if (playerChoice == "paper"){
+        playerChoiceInt = 1;
+    }
+    else if (playerChoice == "scissors")
+    {
+        playerChoiceInt = 2;
+    }
+    compChoiceInt = computerPlay(compChoice);
+    const click = document.querySelector(`button[id="${playerChoice}"]`);
+    click.classList.add('clicked');
+    const clicks = Array.from(document.querySelectorAll('.btn'));
+    clicks.forEach(click => click.addEventListener('transitionend', removeTransition));
+    playGame();
+    })
+
+})
+
+
+
+
+function computerPlay(compChoice){
+    let choiceNum = Math.floor(Math.random() * 3);
+    if (choiceNum == 0){
+        compChoice.Value = "rock";
+    }
+    else if (choiceNum == 1){
+        compChoice.Value = "paper";
+    }
+    else if(choiceNum == 2){
+        compChoice.Value = "scissors";
+    }
+    return choiceNum;
+}
+
+function playRound(){
+        let win_array = [[0, 2, 1], 
+                        [1, 0, 2], 
+                        [2, 1, 0]];
+    let result = win_array[playerChoiceInt][compChoiceInt];
+    if (result == 0){
+    output.textContent = `Its a tie! You chose ${playerChoice} and the computer chose ${compChoice.Value}`;
+    roundCount++;
+    }
+    else if (result == 1){
+    output.textContent = `You won! You chose ${playerChoice} and the computer chose ${compChoice.Value}`;
+    playerScore++;
+    roundCount++;
+
+    }
+    else if (result == 2){
+    output.textContent = `You lost! You chose ${playerChoice} and the computer chose ${compChoice.Value}`;
+    compScore++;
+    roundCount++;
     }
 }
 
-// This allows user to define the game selection 
-// function userInput() {
-//     const ask = prompt("Start game, select: Rock, Paper or Scissors!");
-//     return ask.toLowerCase();
-// }
+function playGame(){
+        reset.textContent = " ";
+        playRound();
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
+        round.textContent = `Round: ${roundCount}`;
 
-// This creates the parameters to determine gameplay result
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return tie;
-        
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper') ||
-        (playerSelection ==='paper' && computerSelection === 'rock')
-      ) {
-        playerScore++;
-        return win;
-    } else if (
-        (computerSelection === 'rock' && playerSelection === 'scissors') ||
-        (computerSelection === 'scissors' && playerSelection === 'paper') ||
-        (computerSelection === 'paper' && playerSelection === 'rock')
-      ) {
-        computerScore++;
-        return lose;
-      }
+        if (playerScore == 5){
+            output.textContent = "You Won the Game!";
+            playerScore = 0;
+            compScore = 0;
+            roundCount = 0;
+            player.textContent = `Player Score: ${playerScore}`;
+            computer.textContent = `Computer Score: ${compScore}`;
+            reset.textContent = "Congrats on the win! Press your luck and try again!";
+            round.textContent = '';
+        }
+        else if (compScore == 5){
+            output.textContent = "You Lost the game";
+            playerScore = 0;
+            compScore = 0;
+            roundCount = 0;
+            player.textContent = `Player Score: ${playerScore}`;
+            computer.textContent = `Computer Score: ${compScore}`;
+            reset.textContent = "The computer was just too good this time! Enact your revenge!";
+            round.textContent = '';
+        }
 }
-
-// Game over
-function gameOver() {
-    if (playerScore === 5 || computerScore === 5) {
-        return "Game Over";
-    } else {
-        return '';
-    } 
-       
-}
-
-// Variables 
-let win = "You won!";
-let lose = "You lost!";
-let tie = "It's a tie!";
-let playerScore = parseInt(0);
-let computerScore = parseInt(0);
-let n = gameOver();
-
-// Loop through rounds
-for (let n = 0; n <= n; n++) {
-    const playerSelection = userInput();
-    const computerSelection = getComputerChoice();
-    console.log("Your Selection:" + " " + playerSelection);
-    console.log("Computer Selection:" + " " + computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    console.log("Your Score:" + " " + playerScore);
-    console.log("Computer's Score:" + " " + computerScore);
-    console.log(gameOver());
-
-    if (playerScore === 5 || computerScore === 5) {
-        break;
-    }
-}
-
